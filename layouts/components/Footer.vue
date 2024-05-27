6b287132e2fc9b6fc4142523c0291696f2089148
jamal161 <jamaluddin.cse@gmail.com>
2024-05-27 11:03:09 +0600

    first commit

---
+++ layouts/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-100 d-flex align-center justify-space-between">
+    <!-- 👉 Footer: left content -->
+    <span class="d-flex align-center">
+      &copy;
+      {{ new Date().getFullYear() }}
+      Content Copyright
+      <VIcon
+        icon="ri-heart-line"
+        color="error"
+        size="1.25rem"
+        class="mx-1"
+      />
+     Sayeed
+    </span>
+   
+  </div>
+</template>
---
+++ layouts/components/NavItems.vue
@@ -0,0 +1,98 @@
+<script setup>
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue'
+import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue'
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+</script>
+
+<template>
+   <NuxtLink
+        to="/"
+        class="app-logo app-title-wrapper"
+      >
+       <VerticalNavGroup
+    :item="{
+      title: 'Dashboards',
+      badgeClass: 'bg-error',
+      icon: 'ri-home-smile-line',
+    }"
+  >
+   
+  </VerticalNavGroup>
+      </NuxtLink>
+
+
+  <VerticalNavLink
+    :item="{
+      title: 'Account Settings',
+      icon: 'ri-user-settings-line',
+      to: '/account-settings',
+    }"
+  />
+
+  <VerticalNavLink
+    :item="{
+      title: 'Login',
+      icon: 'ri-login-box-line',
+      to: '/login',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Register',
+      icon: 'ri-user-add-line',
+      to: '/register',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Error',
+      icon: 'ri-information-line',
+      to: '/no-existence',
+    }"
+  />
+
+  
+  
+  <VerticalNavLink
+    :item="{
+      title: 'Typography',
+      icon: 'ri-text',
+      to: '/typography',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Icons',
+      icon: 'ri-remixicon-line',
+      to: '/icons',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Cards',
+      icon: 'ri-bar-chart-box-line',
+      to: '/cards',
+    }"
+  />
+
+  
+  
+  <VerticalNavLink
+    :item="{
+      title: 'Form Layouts',
+      icon: 'ri-layout-4-line',
+      to: '/form-layouts',
+    }"
+  />
+  
+  <VerticalNavLink
+    :item="{
+      title: 'Tables',
+      icon: 'ri-table-alt-line',
+      to: '/tables',
+    }"
+  />
+
+  
+ 
+</template>
